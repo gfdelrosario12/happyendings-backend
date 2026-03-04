c69e3bb2875e505b895ed034b609479a636a7ca5
Gladwin Ferdz Del Rosario <gladelrosario12@gmail.com>
2026-03-05 03:11:26 +0800

    chore: push setup files

---
+++ src/main/java/com/sparktech/happyendings/HappyendingsApplication.java
@@ -0,0 +1,13 @@
+package com.sparktech.happyendings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyendingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyendingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparktech/happyendings/HappyendingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparktech.happyendings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyendingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
